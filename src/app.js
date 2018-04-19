72c1eaffe3f12ce8a5f79e3691ce4483ba58fa99
Mindjet <pearl920@outlook.com>
2018-04-19 23:55:31 +0800

    Add: add rule for comparing 2 potential arrays

--- src/app.js
+++ src/app.js
@@ -4,9 +4,7 @@ const quizRegister = require('./utils/quiz-register');
 console.log('\n');
 console.log('===== YOUR QUIZ RESULT =====');
 
-quizes.forEach(quiz => {
-    new quiz().checkAll();
-});
+quizes.forEach(quiz => new quiz().checkAll());
 
 console.log('\n');
 console.log('============================');
--- src/utils/equivalent.js
+++ src/utils/equivalent.js
@@ -6,21 +6,44 @@ const equal = (x, y) => {
         };
     }
     let result = false;
+    if (x instanceof Array) {
+        result = arrayEqual(x, y);
+        return {
+            result,
+            reason: y instanceof Array ? 'Both are arrays, but with different elements or structures.' : 'The type of your answer is NOT the same as the correct one.'
+        }
+    }
     switch (typeof x) {
-        case Array:
-            result = arrayEqual(x, y);
         case 'number':
         case 'string':
             result = x === y;
         default:
-            return { result, reason: 'Both are ' + typeof x + " type, but with the different values." }
+            return {
+                result,
+                reason: 'Both are ' + typeof x + " type, but with the different values."
+            }
             break;
     }
 }
 
+//method to compare 2 potential arrays.
 const arrayEqual = (x, y) => {
-
+    if (!x || !y) {
+        return false;
+    }
+    if (x.length !== y.length) {
+        return false;
+    }
+    for (let i = 0; i < x.length; i++) {
+        if ((x[i] instanceof Array) && (y[i] instanceof Array)) {
+            if (!arrayEqual(x[i], y[i])) {
+                return false;
+            }
+        } else if (x[i] !== y[i]) {
+            return false;
+        }
+    }
+    return true;
 }
-
 
 module.exports = { equal };
